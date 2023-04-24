34768f104e6a103449334871dd08586221db8200
ThisIsNana <pw10279@gmail.com>
2023-04-24 12:23:21 +0800

    firstCommit

---
+++ src/test/java/com/example/book_selling/BookSellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_selling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
